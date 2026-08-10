cdde53d7d466d4aaee8c1a84f04a0fe896f64d6f
Eli <elidewitt03@gmail.com>
2026-08-10 10:47:40 -0400

    initialized mlp

---
+++ mlp.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class MLP:
+    def __init__(self, layer_dims : list):
+        self.layer_dims = layer_dims
+
+    def forward_prop(self, input : list):
+        return None
+
+    def backward_prop(self, err : list):
+        return None
